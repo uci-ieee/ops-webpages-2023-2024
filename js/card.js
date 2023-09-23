4cd7ffdce27826dfaae60c57d96d8d5720f32809
Dylan Vu <dylanvu9@gmail.com>
2023-09-22 22:55:56 -0700

    make a slide and video button for each card, and adjust styling a little bit

--- js/card.js
+++ js/card.js
@@ -10,21 +10,28 @@ function createCard() {
     // link/src for image
     const image = document.currentScript.getAttribute('image');
 
-    // link that this card links to, likely an external page or another page
-    const link = document.currentScript.getAttribute('link');
+    // link that this card links to the slides
+    const slides = document.currentScript.getAttribute('slides');
+
+    // link that this card links to the video
+    const video = document.currentScript.getAttribute('video');
+
+    const content = document.currentScript.getAttribute('content');
 
     document.write(`
     <div class="col">
-        <div class="card p-3">
-            <a href="${link}" >
-                <img class="card-img-top" src="${image}" alt="${title}">
-            </a>
+        <div class="card p-3 shadow">
+            <img class="card-img-top" src="${image}" alt="${title}">
             <div class="card-body">
                 <h5 class="card-title">${title}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">
                     ${date}
                 </h6>
-                <a href="${link}" class="card-link">See More</a>
+                <p class="card-text">
+                    ${content}
+                </p>
+                <button class="btn card-btn px-3 ${slides && slides.length > 0 ? "btn-primary" : "btn-secondary disabled"}" onclick="window.location='${slides}';">Slides</button>
+                <button class="btn card-btn px-3 ${video && video.length > 0 ? "btn-primary" : "btn-secondary disabled"}" onclick="window.location='${video}';">Video</button>
             </div>
         </div>
     </div>
